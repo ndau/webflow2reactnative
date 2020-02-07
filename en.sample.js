f7ba400f07ab158d876971df6e64a2b4720f0736
Ed McNierney <ed@mcnierney.com>
2020-02-07 17:25:44 -0500

    Create/fix copyright messages

--- en.sample.js
+++ en.sample.js
@@ -1,3 +1,13 @@
+/* ----- ---- --- -- -
+ * Copyright 2020 The Axiom Foundation. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.apache.org/licenses/LICENSE-2.0.txt
+ * - -- --- ---- -----
+ */
+
 // This is a sample translation file. It must contain an export default object like so but may contain any other code or values. This code will not be run or evaluated, only parsed, so only strings will be used. The values are also not used and only the keys are important. It is assumed the values will be looked up later on in the program by the `i18n.t` function.
 export default {
   send: 'Send',
